cbaea222a90f9a4693cd5d86e6ed0d9602004c8c
GuiMac <guibasconti@gmail.com>
2020-04-02 01:49:13 -0300

    Set Home as a private route

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 /* eslint-disable import/no-named-as-default */
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './routes/PrivateRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
-import PropTypes from 'prop-types';
-import React from 'react';
+import { HOME, LOGIN } from '../constants/routes';
 import { hot } from 'react-hot-loader';
 
 // This is a class-based component because the current
@@ -14,8 +16,8 @@ import { hot } from 'react-hot-loader';
 const App = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
+      <PrivateRoute exact path={HOME} component={Home} />
+      <Route exact path={LOGIN} component={Login} />
       <Route component={NotFound} />
     </Switch>
   );
---
+++ src/components/routes/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import { getSignedIn } from '../../reducers/selectors';
+import { LOGIN } from '../../constants/routes'
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const signedIn = useSelector(getSignedIn);
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        signedIn ? <Component {...props} /> : <Redirect to={LOGIN} />
+      }
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.function
+};
+
+export default PrivateRoute;
