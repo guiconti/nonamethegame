accb46ce7fd3ded992b3300a91e5bed47828b5da
GuiMac <guibasconti@gmail.com>
2020-04-09 03:06:06 -0300

    Add Column and Item components to Create Adventurer Card

--- src/components/elements/adventurer/CreateAdventurer.js
+++ src/components/elements/adventurer/CreateAdventurer.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Column from '../shared/Column';
+import Item from '../shared/Item';
 import Title from '../shared/Title';
 import Avatar from '../shared/Avatar';
 import Card from '../shared/Card';
-import { Grid } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
 import './styles/createAdventurer.scss';
 
 const CreateAdventurer = ({ createAdventurer }) => {
   return (
-    <Grid item align="center" xs={12} sm={6} md={3}>
-      <Card className="create-adventurer__card" onClick={createAdventurer}>
-        <div className="create-adventurer__card-content">
-          <Title>
-            New adventurer!
-          </Title>
+    <Card className="create-adventurer__card" onClick={createAdventurer}>
+      <Column justifyContent="space-between" alignItems="center">
+        <Item xs={4}>
+          <Title className="create-adventurer__title">New adventurer!</Title>
+        </Item>
+        <Item xs={8}>
           <Avatar large outlined className="create-adventurer__avatar">
             <Add fontSize="inherit" />
           </Avatar>
-        </div>
-      </Card>
-    </Grid>
+        </Item>
+      </Column>
+    </Card>
   );
 };
 
 CreateAdventurer.propTypes = {
-  createAdventurer: PropTypes.func.isRequired
+  createAdventurer: PropTypes.func.isRequired,
 };
 
 export default CreateAdventurer;
