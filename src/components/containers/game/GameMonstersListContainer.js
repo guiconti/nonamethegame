7ff7e2bb361f4908d7b1260bb4124f94298e5cb2
GuiMac <guibasconti@gmail.com>
2020-04-11 18:38:00 -0300

    Control target only on server side

--- src/components/containers/game/GameMonstersListContainer.js
+++ src/components/containers/game/GameMonstersListContainer.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { targetMonster } from '../../../actions/gameActions';
-import { updateTarget } from '../../../actions/monsterActions';
 import { getVisibleMonsters, getTarget, getAdventurerId } from '../../../reducers/selectors';
 import MonstersList from '../../elements/monster/MonstersList';
 
@@ -12,20 +11,11 @@ const GameMonstersListContainer = () => {
   const adventurerId = useSelector(getAdventurerId);
   const onMonsterSelect = monsterId => {
     if (monsterId === target) {
-      dispatch(updateTarget(''));
       dispatch(targetMonster(''));
       return;
     }
-    dispatch(updateTarget(monsterId));
     dispatch(targetMonster(monsterId));
   };
-
-  useEffect(() => {
-    const monsterIds = Object.keys(visibleMonsters);
-    if (target && !monsterIds.includes(target)) {
-      dispatch(targetMonster(target));
-    }
-  }, [visibleMonsters]);
 
   return (
     <MonstersList
